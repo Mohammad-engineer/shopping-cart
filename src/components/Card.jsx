efc0de93b25b8291b3a6a4662ca84a7c59fd7944
mohammad <72153665+Mohammad-engineer@users.noreply.github.com>
2024-04-03 15:49:46 +0330

    add loader and design products

---
+++ src/components/Card.jsx
@@ -0,0 +1,24 @@
+import { Link } from "react-router-dom";
+import { HiMiniArrowsPointingOut, HiShoppingCart } from "react-icons/hi2";
+import { shortenText } from "../utils/utils";
+import Styles from "./Card.module.css";
+
+
+
+const Card = ({data}) => {
+    console.log({data});
+    const {category,description,id,image,price,title,rating:{rate,count}} = data
+  return (
+    <div className={Styles.card} >
+        <img src={image} alt={title} />
+        <h3>{shortenText(title)}</h3>
+        <p>{price} $</p>
+        <div className={Styles.actions}>
+            <Link to={`/products/${id}`}><HiMiniArrowsPointingOut /></Link>
+            <button><HiShoppingCart /></button>
+        </div>
+    </div>
+  )
+}
+
+export default Card
--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import Card from "../components/Card";
+import { useProducts } from "../context/ProductContext";
+import Styles from "./ProductsPage.module.css";
+import { InfinitySpin } from "react-loader-spinner";
+import { IoSearch } from "react-icons/io5";
+import { useState } from "react";
+
 
 const ProductsPage = () => {
-  return (
-    <div>ProductsPage</div>
-  )
-}
+  
+  const products = useProducts();
+  const [search,setSearch] = useState('')
 
-export default ProductsPage
+  const searchHandler = ()=>{
+    
+  }
+
+  return (
+    <>
+    <div>
+      <input type="text" placeholder="search..." value={search} onChange={e=>e.target.value.toLocaleLowerCase()}/>
+      <button onClick={searchHandler}><IoSearch /></button>
+    </div>
+      <div className={Styles.container}>
+        <div className={Styles.products}>
+          {!products.length && (
+            <InfinitySpin
+              visible={true}
+              width="200"
+              color="#4fa94d"
+              ariaLabel="infinity-spin-loading"
+            />
+          )}
+          {products.map((product) => (
+            <Card key={product.id} data={product} />
+          ))}
+        </div>
+        <div>sidebar</div>
+      </div>
+    </>
+  );
+};
+
+export default ProductsPage;
